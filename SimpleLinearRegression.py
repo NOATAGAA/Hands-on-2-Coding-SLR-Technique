6829c617cdcc6e0eb7f274bc9b2dd5be934b44ef
NOATAGAA <fernandotrujilloc28@gmail.com>
2024-04-11 18:14:27 -0600

    Hands ON 3 Polynomial regression

--- SimpleLinearRegression.py
+++ SimpleLinearRegression.py
@@ -22,10 +22,10 @@ class SimpleLinearRegression:
         self.r_squared = self.r**2
 
 # Sales (Millon Euro)
-y = [651, 762, 856, 1063, 1190, 1298, 1421, 1440, 1518]
+y = [4,8,12,16,20,24,28,32,36]
 
 # Advertising (Millon Euro)
-x = [23, 26, 30, 34, 43, 48, 52, 57, 58]
+x = [1,2,3,4,5,6,7,8,9]
 
 # Create a SimpleLinearRegression object
 slr = SimpleLinearRegression(x, y)
